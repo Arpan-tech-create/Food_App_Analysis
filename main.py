97d4827ef957e1b3b9dfdd962beeb466b75b3c20
Arpan-tech-create <bhoiarpan123@gmail.com>
2023-02-27 22:52:25 +0530

    First

---
+++ main.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+
+st.header('EDA🍲')
+
+raw=pd.read_csv('indian_food.csv')
+
+st.write(raw)
+
+
+
+st.subheader('Countplot............')
+
+fig = plt.figure(figsize=(10, 4))
+plt.xticks(rotation=60)
+sns.countplot(x="diet",data=raw)
+st.pyplot(fig)
+
+
+val=raw['diet'].value_counts()
+st.sidebar.write(val)
+
+
+
+fig=plt.figure(figsize=(10,5))
+sns.barplot(x=raw['course'].value_counts().index,y=raw['course'].value_counts())
+st.pyplot(fig)
+bar=raw['course'].value_counts()
+st.sidebar.write(bar)
+
